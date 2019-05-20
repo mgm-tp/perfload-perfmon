c20637f1c9bc5ae04911f1245f557b2fefbf9aca
Andreas Mueller <andreas.mueller@mgm-tp.com>
2019-05-20 16:15:23 +0200

    add increment calculation to tcp values because they are OS counters as much as diskio counters
    Change-Id: I54550fd40a6c1c78681a1561c623ea469e7b1549

--- src/main/java/com/mgmtp/perfload/perfmon/cmd/PerfMonTcp.java
+++ src/main/java/com/mgmtp/perfload/perfmon/cmd/PerfMonTcp.java
@@ -27,6 +27,11 @@ public class PerfMonTcp extends BasePerfMonCommand {
 
 	private static final String TYPE_TCP = "tcp";
 
+	// The tcp attributes (with the exception of 'currEstab') are strictly growing OS counters.
+	// To measure the growth of those counters we need to store the most recent values as 'offsets'
+	// for the next time interval
+	private TcpData offset = new TcpData();
+
 	public PerfMonTcp(final String separator) {
 		super(separator);
 	}
@@ -35,41 +40,121 @@ public class PerfMonTcp extends BasePerfMonCommand {
 	public String executeCommand(final SigarProxy sigar) throws SigarException {
 		StrBuilder sb = new StrBuilder(200);
 
-		Tcp tcp = sigar.getTcp();
-
-		long activeOpens = tcp.getActiveOpens();
-		long passiveOpens = tcp.getPassiveOpens();
-		long attemptFails = tcp.getAttemptFails();
-		long estabResets = tcp.getEstabResets();
-		long currEstab = tcp.getCurrEstab();
-		long inSegs = tcp.getInSegs();
-		long outSegs = tcp.getOutSegs();
-		long retransSegs = tcp.getRetransSegs();
-		long inErrs = tcp.getInErrs();
-		long outRsts = tcp.getOutRsts();
+		// get counter data from sigar
+		TcpData counterData = new TcpData(sigar.getTcp());
+		// calculate the data to write to the report by subtracting the offset
+		TcpData reportData = counterData.cloneAndSubtractOffset(offset);
+		// store the original counter data as new offset
+		offset = counterData;
 
 		sb.append(TYPE_TCP);
 		sb.append(separator);
-		sb.append(activeOpens); // active connections openings
+		sb.append(reportData.activeOpens); // active connections openings
 		sb.append(separator);
-		sb.append(passiveOpens); // passive connection openings
+		sb.append(reportData.passiveOpens); // passive connection openings
 		sb.append(separator);
-		sb.append(attemptFails); // failed connection attempts
+		sb.append(reportData.attemptFails); // failed connection attempts
 		sb.append(separator);
-		sb.append(estabResets); // connection resets received
+		sb.append(reportData.estabResets); // connection resets received
 		sb.append(separator);
-		sb.append(currEstab);// connections established
+		sb.append(reportData.currEstab);// connections established
 		sb.append(separator);
-		sb.append(inSegs); // segments received
+		sb.append(reportData.inSegs); // segments received
 		sb.append(separator);
-		sb.append(outSegs); // segments send out
+		sb.append(reportData.outSegs); // segments send out
 		sb.append(separator);
-		sb.append(retransSegs); // segments retransmitted
+		sb.append(reportData.retransSegs); // segments retransmitted
 		sb.append(separator);
-		sb.append(inErrs); // bad segments received
+		sb.append(reportData.inErrs); // bad segments received
 		sb.append(separator);
-		sb.append(outRsts); // resets sent
+		sb.append(reportData.outRsts); // resets sent
 
 		return sb.toString();
+	}
+
+	private static class TcpData {
+		private boolean isEmpty=true;
+		private long activeOpens=0;
+		private long passiveOpens=0;
+		private long attemptFails=0;
+		private long estabResets=0;
+		private long currEstab=0;
+		private long inSegs=0;
+		private long outSegs=0;
+		private long retransSegs=0;
+		private long inErrs=0;
+		private long outRsts=0;
+
+		//create empty with all values -1
+		public TcpData() {};
+
+		// create from sigar tcp object
+		public TcpData(Tcp tcp) {
+			this.activeOpens = tcp.getActiveOpens();
+			this.passiveOpens = tcp.getPassiveOpens();
+			this.attemptFails = tcp.getAttemptFails();
+			this.estabResets = tcp.getEstabResets();
+			this.currEstab = tcp.getCurrEstab();
+			this.inSegs = tcp.getInSegs();
+			this.outSegs = tcp.getOutSegs();
+			this.retransSegs = tcp.getRetransSegs();
+			this.inErrs = tcp.getInErrs();
+			this.outRsts = tcp.getOutRsts();
+			this.isEmpty=false;
+		}
+
+		/**
+		 * clone data
+		 */
+		public TcpData(TcpData oData) {
+			if( !oData.isEmpty ) {
+				this.activeOpens = oData.activeOpens;
+				this.passiveOpens = oData.passiveOpens;
+				this.attemptFails = oData.attemptFails;
+				this.estabResets = oData.estabResets;
+				this.currEstab = oData.currEstab;
+				this.inSegs = oData.inSegs;
+				this.outSegs = oData.outSegs;
+				this.retransSegs = oData.retransSegs;
+				this.inErrs = oData.inErrs;
+				this.outRsts = oData.outRsts;
+				this.isEmpty=oData.isEmpty;
+			}
+		}
+
+		/**
+		 * clone the data and subtract the offset in a single op
+		 *
+		 * @return cloned data with offset subtracted
+		 * @return 0 data if the offset is empty
+		 * @throws IllegalStateException if the data to clone are empty
+		 */
+		public TcpData cloneAndSubtractOffset(TcpData offset) {
+			if( !this.isEmpty ) {
+				TcpData delta;
+				if( offset.isEmpty ) {
+					delta = new TcpData();
+					delta.isEmpty=false;
+					// it's not a counter:
+					delta.currEstab=this.currEstab;
+				} else {
+					delta = new TcpData(this);
+					delta.activeOpens -= offset.activeOpens;
+					delta.passiveOpens -= offset.passiveOpens;
+					delta.attemptFails -= offset.attemptFails;
+					delta.estabResets -= offset.estabResets;
+					//currEstab is not a counter: do not subtract from current value
+					//delta.currEstab -= offset.currEstab;
+					delta.inSegs -= offset.inSegs;
+					delta.outSegs -= offset.outSegs;
+					delta.retransSegs -= offset.retransSegs;
+					delta.inErrs -= offset.inErrs;
+					delta.outRsts -= offset.outRsts;
+				}
+				return delta;
+			} else {
+				throw new IllegalStateException("must not clone empty data");
+			}
+		}
 	}
 }
